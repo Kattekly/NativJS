facc662458667f66d71667c108f2471767d99f8d
Ekaterina <kattekly@mail.ru>
2022-09-28 15:41:58 +0300

    Lessons-07

--- src/06/destructuring.test.tsx
+++ src/06/destructuring.test.tsx
@@ -1,9 +1,34 @@
-test('', ()=> {
 
-    let man = {
+
+type ManType2 = {
+    name: string
+    age: number
+    lessons: Array<{title: string}>,
+    address: {
+        street: {
+            title: string
+}}}
+
+let man: ManType2;
+
+beforeEach(() => {
+    man = {
         name: 'Kate',
         age: 26,
-        lessons: [{title: 1},{title: 2}]
+        lessons: [{title: "1"}, {title: "2"}],
+        address: {
+            street: {
+                title: 'Kalina'
+            }
+        }
     }
+})
 
+test('', () => {
+
+    const {age, lessons} = man
+    const {title} = man.address.street
+    expect(age).toBe(26)
+    expect(lessons.length).toBe(2)
+    expect(title).toBe('Kalina')
 })
