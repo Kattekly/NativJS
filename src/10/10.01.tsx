05ca5bef92a15d3089c10d97dd915accf6cd090e
Ekaterina <kattekly@mail.ru>
2023-01-22 19:10:43 +0300

    Lessons-10

--- src/10/10.01.tsx
+++ src/10/10.01.tsx
@@ -25,7 +25,7 @@ export type WithCompaniesType = {
     companies: Array<CompanyType>
 }
 
-type CompanyType = {id: number, title: string}
+type CompanyType = { id: number, title: string }
 
 
 export function icreaseAge(u: ManType, power: number) {
@@ -83,53 +83,53 @@ export function addNewBookUser(u: UserHasLaptopType & UserWithBooksType, newBook
 }
 
 export function updateBookUser(u: UserHasLaptopType & UserWithBooksType, oldBooks: string, updateBooks: string) {
-   return {
-       ...u,
-       books: u.books.map(b => b === oldBooks ? updateBooks: b)
-   }
-
-   /* return {
-        ...u,
-        books: u.books.map(b => {
-            if (b === oldBooks) {
-                return updateBooks
-            } else {
-                return b
-            }
-        })
-    }*/
-}
-
-export function updateSkillsUser (u: UserHasLaptopType & UserWithBooksType & SkillsManType, oldSkil: number, newSkil: number) {
     return {
         ...u,
-        skills: u.skills.map(b => b === oldSkil ? newSkil: b)
+        books: u.books.map(b => b === oldBooks ? updateBooks : b)
+    }
+
+    /* return {
+         ...u,
+         books: u.books.map(b => {
+             if (b === oldBooks) {
+                 return updateBooks
+             } else {
+                 return b
+             }
+         })
+     }*/
+}
+
+export function updateSkillsUser(u: UserHasLaptopType & UserWithBooksType & SkillsManType, oldSkil: number, newSkil: number) {
+    return {
+        ...u,
+        skills: u.skills.map(b => b === oldSkil ? newSkil : b)
     }
 }
 
-export function removeBookUser (u: UserHasLaptopType & UserWithBooksType, book: string) {
+export function removeBookUser(u: UserHasLaptopType & UserWithBooksType, book: string) {
     return {
         ...u,
         books: u.books.filter(b => b !== book)
     }
 }
 
-export function addNewCompanyUser (u: UserHasLaptopType & UserWithBooksType & WithCompaniesType, newCompany:  {id: number, title: string}) {
+export function addNewCompanyUser(u: UserHasLaptopType & UserWithBooksType & WithCompaniesType, newCompany: { id: number, title: string }) {
     return {
         ...u,
         companies: [...u.companies, newCompany]
     }
 }
 
-export function updateNewCompanyUser (u: UserHasLaptopType & UserWithBooksType & WithCompaniesType, id: number, newName: string) {
+export function updateNewCompanyUser(u: UserHasLaptopType & UserWithBooksType & WithCompaniesType, id: number, newName: string) {
     return {
         ...u,
-        companies: u.companies.map(b => b.id === id ? {...b, title: newName}: b)
+        companies: u.companies.map(b => b.id === id ? {...b, title: newName} : b)
     }
 }
 
-export function updateNewCompany (companies: {[key: string]: Array<CompanyType> }, userName: string, companyID: number, newTitle: string) {
-  let companyCopy = {...companies}
+export function updateNewCompany(companies: { [key: string]: Array<CompanyType> }, userName: string, companyID: number, newTitle: string) {
+    let companyCopy = {...companies}
     companyCopy[userName] = companyCopy[userName].map(c => c.id === companyID ? {...c, title: newTitle} : c)
 
     return companyCopy
